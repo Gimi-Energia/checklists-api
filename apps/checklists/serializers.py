6b437a3fe5964256dc63558af1add0b8d2f2e9ee
Felipe <lipe.h.r@gmail.com>
2024-03-19 15:37:48 -0300

    :sparkles: feat: send email when a checklist is created

--- apps/checklists/serializers.py
+++ apps/checklists/serializers.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.core.mail import send_mail
 from rest_framework import serializers
 
 from .models import Checklist, ChecklistProduct, Product
@@ -25,17 +27,64 @@ class ChecklistSerializer(serializers.ModelSerializer):
         fields = "__all__"
 
     def create(self, validated_data):
-        products_data = validated_data.pop(
-            "checklistproduct_set", []
-        )
+        products_data = validated_data.pop("checklistproduct_set", [])
         checklist = Checklist.objects.create(**validated_data)
         for product_data in products_data:
-            product = Product.objects.get(
-                id=product_data["product"]["id"]
-            )
+            product = Product.objects.get(id=product_data["product"]["id"])
             ChecklistProduct.objects.create(
                 checklist=checklist, product=product, status=product_data["status"]
             )
+
+        subject = "Grupo Gimi - Novo Checklist Criado"
+        message = f"""
+        <!DOCTYPE html>
+        <html>
+        <head>
+            <style>
+                body {{
+                    font-family: Arial, sans-serif;
+                    font-size: 16px;
+                }}
+                .btn {{
+                    display: inline-block;
+                    background-color: #f0f0f0;
+                    padding: 8px 16px;
+                    text-align: center;
+                    text-decoration: none;
+                    font-size: 16px;
+                    border-radius: 10px;
+                    margin-top: 10px;
+                    border: 2px solid black;
+                    font-weight: bold;
+                }}
+                .code {{
+                    background-color: #f0f0f0;
+                    padding: 2px 4px;
+                    border-radius: 4px;
+                    font-family: monospace;
+                    font-weight: bold;
+                }}
+            </style>
+        </head>
+        <body>
+            <p>Olá <strong>{checklist.client_name}</strong>!</p>
+            <p>Um novo checklist do Grupo Gimi foi enviado. 🎉</p>
+            <p>Insira o ID <span class="code">{checklist.id}</span> em nosso webapp para prosseguir.</p>
+            <a href="https://www.google.com/" target="_blank" class="btn">Acessar Webapp</a>
+        </body>
+        </html>
+        """
+        email_from = settings.EMAIL_HOST_USER
+        recipient_list = [checklist.client_email]
+        send_mail(
+            subject=subject,
+            message=f"Cole o ID: {checklist.id} em nosso webapp",
+            from_email=email_from,
+            recipient_list=recipient_list,
+            html_message=message,
+            fail_silently=False,
+        )
+
         return checklist
 
     def update(self, instance, validated_data):
