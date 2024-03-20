e8a99868bd90089e23ee68ae021d7220bf14e53d
Felipe <lipe.h.r@gmail.com>
2024-03-20 16:19:43 -0300

    :sparkles: feat: add registrations admin

--- apps/registrations/admin.py
+++ apps/registrations/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Registration
+
+
+class RegistrationAdmin(admin.ModelAdmin):
+    list_display = ("id", "process_number")
+
+
+admin.site.register(Registration, RegistrationAdmin)
