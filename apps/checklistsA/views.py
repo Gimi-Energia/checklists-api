d9fcd67d4776aadf98369abbc8d711186ee144b4
Felipe <lipe.h.r@gmail.com>
2024-04-29 10:40:53 -0300

    :sparkles: feat: implement exceptions to breakers and transformers quantities

--- apps/checklistsA/views.py
+++ apps/checklistsA/views.py
@@ -1,4 +1,6 @@
-from rest_framework import viewsets
+from rest_framework import status, viewsets
+from rest_framework.response import Response
+
 from .models import ChecklistA
 from .serializers import ChecklistASerializer
 
@@ -6,3 +8,26 @@ from .serializers import ChecklistASerializer
 class ChecklistAViewSet(viewsets.ModelViewSet):
     queryset = ChecklistA.objects.all()
     serializer_class = ChecklistASerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            transformers_count = len(request.data.get("transformers", []))
+            current_transformers_count = len(request.data.get("current_transformers", []))
+
+            if serializer.validated_data["transformers_quantity"] != transformers_count:
+                return Response(
+                    {"error": "Transformers quantity do not match their respective count."},
+                    status=status.HTTP_400_BAD_REQUEST,
+                )
+            elif serializer.validated_data["breakers_quantity"] != current_transformers_count:
+                return Response(
+                    {"error": "Breakers quantity do not match their respective count."},
+                    status=status.HTTP_400_BAD_REQUEST,
+                )
+
+            serializer.save()
+            headers = self.get_success_headers(serializer.data)
+            return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
