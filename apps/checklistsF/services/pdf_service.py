b3cac2e46ad825871569b3ddbb01ff3c457ac981
Felipe <lipe.h.r@gmail.com>
2024-07-26 12:49:52 -0300

    :bug: fix: adjust pdf F

--- apps/checklistsF/services/pdf_service.py
+++ apps/checklistsF/services/pdf_service.py
@@ -32,10 +32,6 @@ def generate_pdf(instance, transformers_data, current_transformers_data):
     elements.append(Spacer(1, 0.2 * inch))
 
     details_cabin = [
-        Paragraph(
-            f"<b>Concessionária:</b> {instance.concessionaire if instance.concessionaire else instance.other_concessionarie}",
-            styles["Normal"],
-        ),
         Paragraph(f"<b>Tensão Primária:</b> {instance.primary_voltage} kV", styles["Normal"]),
         Paragraph(
             f"<b>Uso do painel:</b> {translations.get(instance.panel_usage)}", styles["Normal"]
@@ -44,96 +40,11 @@ def generate_pdf(instance, transformers_data, current_transformers_data):
             f"<b>Lado da Entrada dos Cabos:</b> {translations.get(instance.cable_side)}",
             styles["Normal"],
         ),
-        Paragraph(f"<b>Demanda Contratada:</b> {instance.contracted_demand} kW", styles["Normal"]),
         Paragraph(
             f"<b>Quantidade de Transformadores:</b> {instance.transformers_quantity}",
             styles["Normal"],
         ),
     ]
-
-    details_contractor = [
-        Paragraph(f"<b>Nome do Contratante:</b> {instance.contractor_name}", styles["Normal"]),
-        Paragraph(
-            f"<b>Documento do Contratante:</b> {instance.contractor_document}", styles["Normal"]
-        ),
-        Paragraph(
-            f"<b>Contato do Contratante:</b> {instance.contractor_contact}", styles["Normal"]
-        ),
-        Paragraph(f"<b>Telefone do Contratante:</b> {instance.contractor_phone}", styles["Normal"]),
-        Paragraph(f"<b>Rua do Contratante:</b> {instance.contractor_street}", styles["Normal"]),
-        Paragraph(f"<b>Número do Contratante:</b> {instance.contractor_number}", styles["Normal"]),
-        Paragraph(
-            f"<b>Bairro do Contratante:</b> {instance.contractor_neighborhood}", styles["Normal"]
-        ),
-        Paragraph(f"<b>Cidade do Contratante:</b> {instance.contractor_city}", styles["Normal"]),
-        Paragraph(f"<b>Estado do Contratante:</b> {instance.contractor_state}", styles["Normal"]),
-        Paragraph(
-            f"<b>CEP/Coordenadas do Contratante:</b> {instance.contractor_zip_code}",
-            styles["Normal"],
-        ),
-    ]
-
-    if instance.contractor_complement:
-        details_contractor.append(
-            Paragraph(
-                f"<b>Complemento do Contratante:</b> {instance.contractor_complement}",
-                styles["Normal"],
-            )
-        )
-
-    details_owner_work = [
-        Paragraph(f"<b>Nome do Proprietário:</b> {instance.owner_name}", styles["Normal"]),
-        Paragraph(f"<b>Documento do Proprietário:</b> {instance.owner_document}", styles["Normal"]),
-        Paragraph(f"<b>Contato do Proprietário:</b> {instance.owner_contact}", styles["Normal"]),
-        Paragraph(f"<b>Telefone do Proprietário:</b> {instance.owner_phone}", styles["Normal"]),
-        Paragraph(f"<b>Rua da Obra:</b> {instance.work_street}", styles["Normal"]),
-        Paragraph(f"<b>Número da Obra:</b> {instance.work_number}", styles["Normal"]),
-        Paragraph(f"<b>Bairro da Obra:</b> {instance.work_neighborhood}", styles["Normal"]),
-        Paragraph(f"<b>Cidade da Obra:</b> {instance.work_city}", styles["Normal"]),
-        Paragraph(f"<b>Estado da Obra:</b> {instance.work_state}", styles["Normal"]),
-        Paragraph(f"<b>CEP/Coordenadas da Obra:</b> {instance.work_zip_code}", styles["Normal"]),
-    ]
-
-    if instance.work_complement:
-        details_owner_work.append(
-            Paragraph(
-                f"<b>Complemento da Obra:</b> {instance.work_complement}",
-                styles["Normal"],
-            )
-        )
-
-    details_fire_exit = [
-        Paragraph(
-            f"<b>Saída Exclusiva para Transformador de Incêndio:</b> {'Sim' if instance.fire_exit else 'Não'}",
-            styles["Normal"],
-        ),
-    ]
-
-    if instance.fire_exit:
-        details_fire_exit.append(
-            Paragraph(
-                f"<b>Potência:</b> {instance.fire_transformer_power} kVA",
-                styles["Normal"],
-            )
-        )
-        details_fire_exit.append(
-            Paragraph(
-                f"<b>Demanda:</b> {instance.fire_transformer_demand} kW",
-                styles["Normal"],
-            )
-        )
-        details_fire_exit.append(
-            Paragraph(
-                f"<b>Impedância:</b> {instance.fire_transformer_impedance} %",
-                styles["Normal"],
-            )
-        )
-        details_fire_exit.append(
-            Paragraph(
-                f"<b>Tipo:</b> {translations.get(instance.fire_transformer_type)}",
-                styles["Normal"],
-            )
-        )
 
     details_study = [
         Paragraph(
@@ -221,16 +132,6 @@ def generate_pdf(instance, transformers_data, current_transformers_data):
             elements.append(Spacer(1, 0.1 * inch))
 
     elements.append(Spacer(1, 0.2 * inch))
-    subtitle_fire_exit = "Dados Transformador de Incêndio"
-    subtitle_fire_exit_para = Paragraph(subtitle_fire_exit, subtitle_style)
-    elements.append(subtitle_fire_exit_para)
-    elements.append(Spacer(1, 0.2 * inch))
-
-    for detail in details_fire_exit:
-        elements.append(detail)
-        elements.append(Spacer(1, 0.1 * inch))
-
-    elements.append(Spacer(1, 0.2 * inch))
     subtitle_study = "Dados do Estudo"
     subtitle_study_para = Paragraph(subtitle_study, subtitle_style)
     elements.append(subtitle_study_para)
@@ -257,26 +158,6 @@ def generate_pdf(instance, transformers_data, current_transformers_data):
                 )
                 elements.append(paragraph)
                 elements.append(Spacer(1, 0.1 * inch))
-
-    elements.append(Spacer(1, 0.2 * inch))
-    subtitle_contractor = "Dados Contrato"
-    subtitle_contractor_para = Paragraph(subtitle_contractor, subtitle_style)
-    elements.append(subtitle_contractor_para)
-    elements.append(Spacer(1, 0.2 * inch))
-
-    for detail in details_contractor:
-        elements.append(detail)
-        elements.append(Spacer(1, 0.1 * inch))
-
-    elements.append(Spacer(1, 0.2 * inch))
-    subtitle_owner_work_ = "Dados Obra / Serviço"
-    subtitle_owner_work__para = Paragraph(subtitle_owner_work_, subtitle_style)
-    elements.append(subtitle_owner_work__para)
-    elements.append(Spacer(1, 0.2 * inch))
-
-    for detail in details_owner_work:
-        elements.append(detail)
-        elements.append(Spacer(1, 0.1 * inch))
 
     document.build(elements)
 
