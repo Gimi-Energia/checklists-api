7fda8a300232c7a5dd03cd696db143a9543b8f56
Felipe <lipe.h.r@gmail.com>
2024-09-11 10:43:16 -0300

    :sparkles: feat: complete control of users in admin

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,17 +1,50 @@
 from django.contrib import admin
-from apps.users.models import User
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+
+from .models import User
 
 
-class Users(admin.ModelAdmin):
-    list_display = ("id", "name", "email", "company")
-    list_display_links = ("id",)
-    search_fields = (
-        "name",
-        "email",
+class UserAdmin(BaseUserAdmin):
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        (
+            "Personal Info",
+            {
+                "fields": (
+                    "id",
+                    "name",
+                    "company",
+                )
+            },
+        ),
+        (
+            "Permissions",
+            {
+                "fields": (
+                    "is_active",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                )
+            },
+        ),
+        ("Important dates", {"fields": ("last_login", "date_joined")}),
     )
-    list_filter = ("is_admin",)
-    list_per_page = 25
-    ordering = ("name", "email")
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("email", "password1", "password2"),
+            },
+        ),
+    )
+    list_display = ("id", "email", "name", "company")
+    search_fields = ("email", "name", "company")
+    ordering = ("email",)
+    readonly_fields = ["id", "date_joined"]
+
+    list_filter = ("company", "is_superuser")
 
 
-admin.site.register(User, Users)
+admin.site.register(User, UserAdmin)
