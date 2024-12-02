b620ebb18fc8a1c60feed8af256b984844a6db95
fehenriq <lipe.h.r@gmail.com>
2024-12-02 10:27:06 -0300

    :bug: fix: increase max_length of fields

--- apps/checklistsB/models.py
+++ apps/checklistsB/models.py
@@ -48,9 +48,9 @@ class ChecklistB(BaseModel):
     transformer_power = models.FloatField(_("Transformer Power"))
     manufacturer_project = models.BooleanField(_("Has a Manufacturer and Project?"), default=True)
 
-    contractor_name = models.CharField(_("Contractor Name"), max_length=50)
+    contractor_name = models.CharField(_("Contractor Name"), max_length=70)
     contractor_document = models.CharField(_("Contractor Document"), max_length=18)
-    contractor_contact = models.CharField(_("Contractor Contact"), max_length=50)
+    contractor_contact = models.CharField(_("Contractor Contact"), max_length=70)
     contractor_phone = models.CharField(_("Contractor Phone"), max_length=15)
     contractor_street = models.CharField(_("Contractor Street"), max_length=255)
     contractor_number = models.CharField(_("Contractor Number"), max_length=20)
@@ -66,9 +66,9 @@ class ChecklistB(BaseModel):
     contractor_latitude = models.FloatField(_("Contractor Latitude"), blank=True, null=True)
     contractor_longitude = models.FloatField(_("Contractor Longitude"), blank=True, null=True)
 
-    owner_name = models.CharField(_("Owner Name"), max_length=50)
+    owner_name = models.CharField(_("Owner Name"), max_length=70)
     owner_document = models.CharField(_("Owner Document"), max_length=18)
-    owner_contact = models.CharField(_("Owner Contact"), max_length=50)
+    owner_contact = models.CharField(_("Owner Contact"), max_length=70)
     owner_phone = models.CharField(_("Owner Phone"), max_length=15)
     work_street = models.CharField(_("Work Street"), max_length=255)
     work_number = models.CharField(_("Work Number"), max_length=20)
